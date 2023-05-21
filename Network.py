fba4f7804fb7f266a0c56d2c13daff70884976f9
mm2319 <mm2319@ic.ac.uk>
2023-05-21 15:54:53 +0100

    1

--- Network.py
+++ Network.py
@@ -11,21 +11,27 @@ from keras.optimizers import SGD, Adam
 class FeedForwardNetwork(tf.keras.Model):
     def __init__(self, output_size,name=None):
         super().__init__(name=name)
-        # this neural network contains 5 layers
-        self.dense_1 = tf.keras.layers.Dense(1024, activation="selu")
-        self.dropout_1 = tf.keras.layers.Dropout(0.5)
-        self.dense_2 = tf.keras.layers.Dense(2048, activation="selu")
-        self.dropout_2 = tf.keras.layers.Dropout(0.5)
-        self.dense_3 = tf.keras.layers.Dense(1024, activation="selu")
-        self.dropout_3 = tf.keras.layers.Dropout(0.5)
-        self.dense_4 = tf.keras.layers.Dense(output_size, activation="selu")
+        # this neural network contains 8 layers
+        self.dense_1 = tf.keras.layers.Dense(1, activation="selu")
+        self.dense_2 = tf.keras.layers.Dense(64, activation="selu")
+        self.dense_3 = tf.keras.layers.Dense(128, activation="selu")
+        self.dense_4 = tf.keras.layers.Dense(256, activation="selu")
+        self.dense_5 = tf.keras.layers.Dense(1024, activation="selu")
+        self.dropout = tf.keras.layers.Dropout(0.5)
+        self.dense_6 = tf.keras.layers.Dense(256, activation="selu")
+        self.dense_7 = tf.keras.layers.Dense(128, activation="selu")
+        self.dense_8 = tf.keras.layers.Dense(output_size, activation="selu")
 
     def __call__(self, x):
         x = self.dense_1(x)
-        x = self.dropout_1(x)
+        x = self.dropout(x)
         x = self.dense_2(x)
-        x = self.dropout_2(x)
         x = self.dense_3(x)
-        x = self.dropout_3(x)
         x = self.dense_4(x)
+        x = self.dense_5(x)
+        x = self.dense_6(x)
+        x = self.dropout(x)
+        x = self.dense_7(x)
+        x = self.dropout(x)
+        x = self.dense_8(x)
         return x
